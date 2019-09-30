626c19ddcc3a054393d996ff714f68163a279d80
danielevans-999 <danielevans.karani@gmail.comgit>
2019-09-30 10:51:38 +0300

    This adds the paths of different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { UserReposComponent } from './user-repos/user-repos.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path:'home', component:HomeComponent},
+ {path:'user',component:UserProfileComponent},
+ {path:'repos',component:UserReposComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
