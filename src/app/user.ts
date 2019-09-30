0e9c95308161257889df8af6558fd755743f16c6
danielevans-999 <danielevans.karani@gmail.comgit>
2019-09-30 12:23:24 +0300

    Adds a property

--- src/app/user.ts
+++ src/app/user.ts
@@ -6,7 +6,8 @@ export class User {
     public avatar_url: string,
     public public_repos: number,
     public followers: number,
-    public following: number
+    public following: number,
+    public created_at:Date,
   ) { 
 
   }
