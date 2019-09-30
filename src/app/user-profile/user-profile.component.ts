1432e67e420e247888d00bbeb9bec7258bd667d3
danielevans-999 <danielevans.karani@gmail.comgit>
2019-09-30 03:48:39 +0300

    Injected the service and injected it on the constructor

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RequestsService} from '../requests.service'
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  profile:any
 
-  constructor() { }
+  constructor(private userService:RequestsService) { }
 
   ngOnInit() {
   }
